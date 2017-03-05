cac0485a00467fa07556afd859130de3be5db81a
Chris Fontenot <chris.fontenot@gmail.com>
2017-03-05 11:54:32 -0600

    updated config file and added 3 working scripts
    The config file is built out a little more with some beginning
    parameters. The three php scripts need valid values added for the
    apikey and then each may have some other field like username or GUID
    that should be completed with valid values.

---
+++ ethosProxyGetPersonByGuid.php
@@ -0,0 +1,65 @@
+<?php
+header('Content-Type: application/json');
+
+$apikey="ey-your-api-key-here";
+
+// Setting up ethos specific details
+$ethosurl='https://integrateapi.elluciancloud.com';
+$authurlpath='/auth';
+$apiurlpath='/api/';
+$ethosDataModel="persons";
+$ethosguid="1111-2222-3333-4444-555";
+//TODO Add information to find the GUIDs in the Ellucian ERP systems
+//$publishurl='https://integrateapi.elluciancloud.com/publish';
+//$checkurl='https://integrateapi.elluciancloud.com/consume';
+//$consumeurl='https://integrateapi.elluciancloud.com/consume?lastProcessedID=0&max=1';
+
+// Header options for authentication request
+$authOpts = array(
+  'http'=>array(
+    'method'=>"POST",
+    'header'=>"Authorization: Bearer ".$apikey."\r\n" .
+              "Content-Type: application/json\r\n" .
+              "charset: UTF-8"
+  )
+);
+
+$authContext = stream_context_create($authOpts);
+
+try {
+  $authtoken = file_get_contents($ethosurl.$authurlpath, false, $authContext);
+  if ($authtoken === false){
+    echo "\r\nWe must have done something wrong because our attempt returned false\r\n";
+  } else {
+    //echo "The authorization token is: ".$authtoken;
+  }
+} catch (Exception $e) {
+  echo "We caught an exeption: " . $e . "\r\n\r\n";
+}
+
+// Header options for proxy GET request
+$proxyGetOpts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Authorization: Bearer ".$authtoken."\r\n" .
+              "Content-Type: application/vnd.hedtech.s.v2.json\r\n" .
+              "Accept: application/json",
+    'timeout' => 60
+  )
+);
+
+$proxyGetContext = stream_context_create($proxyGetOpts);
+
+// Lets use the authtoken to get some data
+try {
+  $getData = file_get_contents($ethosurl.$apiurlpath.$ethosDataModel."/".$ethosguid, false, $proxyGetContext);
+  if ($getData === false){
+    echo "\r\nWe must have done something wrong because our attempt returned false\r\n";
+  } else {
+    echo json_encode(json_decode($getData), JSON_PRETTY_PRINT);
+  }
+} catch (Exception $e) {
+  echo "We caught an exeption: " . $e . "\r\n";
+}
+
+?>
