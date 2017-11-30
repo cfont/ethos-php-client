dc55d74281f421a0a65186fa7d8b7032510b15b6
Chris Fontenot <chris.fontenot@gmail.com>
2017-11-30 10:09:14 -0600

    adding search form examples
    created new directory called “searchx” to store new examples, created
    two new files for processing searches through Ethos, added index file
    with the search forms that provide easy examples for persons data model

---
+++ searchx/ethosSearchPersonsByCredential.php
@@ -0,0 +1,70 @@
+<?php
+header('Content-Type: application/json');
+
+$apikey="ey-your-api-key-here";
+
+
+// Don't change these URLs
+$ethosurl='https://integrateapi.elluciancloud.com';
+$authurlpath='/auth';
+$apiurlpath='/api/';
+$ethosDataModel="persons";
+$credentialID=$_GET["credentialValue"];
+$credentialType=$_GET["credentialType"];
+//TODO: provide options and documentation for credentialType
+//$publishurl='https://integrateapi.elluciancloud.com/publish';
+//$checkurl='https://integrateapi.elluciancloud.com/consume';
+//$consumeurl='https://integrateapi.elluciancloud.com/consume?lastProcessedID=0&max=1';
+
+// Header options for authentication request
+$authOpts = array(
+  'http'=>array(
+    'method'=>"POST",
+    'header'=>"Authorization: Bearer ".$apikey."\r\n" .
+              "Content-Type: application/json\r\n" .
+              "charset: UTF-8"
+  )
+);
+
+$authContext = stream_context_create($authOpts);
+
+try {
+  $authtoken = file_get_contents($ethosurl.$authurlpath, false, $authContext);
+  if ($authtoken === false){
+    echo "\r\nWe must have done something wrong because our attempt returned false\r\n";
+  } else {
+    //echo "The authorization token is: ".$authtoken;
+  }
+} catch (Exception $e) {
+  echo "We caught an exeption: " . $e . "\r\n\r\n";
+}
+
+// Header options for proxy GET request
+$proxyGetOpts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Authorization: Bearer ".$authtoken."\r\n" .
+              "Content-Type: application/vnd.hedtech.s.v2.json\r\n" .
+              "Accept: application/json",
+    'timeout' => 60
+  )
+);
+
+$proxyGetContext = stream_context_create($proxyGetOpts);
+
+// Lets use the authtoken to get some data
+
+try {
+  $personSearchCriteria = array("credential" => array("type" => "$credentialType", "value" => "$credentialID"));
+  $getData = file_get_contents($ethosurl.$apiurlpath.$ethosDataModel."?criteria=".json_encode($personSearchCriteria), false, $proxyGetContext);
+  if ($getData === false){
+    echo "\r\nWe must have done something wrong because our attempt returned false\r\n";
+  } else {
+    echo json_encode(json_decode($getData), JSON_PRETTY_PRINT);
+
+  }
+} catch (Exception $e) {
+  echo "We caught an exeption: " . $e . "\r\n";
+}
+
+?>
